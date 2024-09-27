bb16dbda29019682df265d9e979c3f9df8455890
lgz <3046271576@qq.com>
2024-09-27 13:25:48 +0800

    first page

--- game/views.py
+++ game/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    return HttpResponse("我的第一个页面！！！")
+
+def play(request):
+    return HttpResponse("游戏界面")
