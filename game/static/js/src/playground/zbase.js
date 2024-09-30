1009ac2942da1c4855e74fb94678dcfb2ba327b1
lgz <3046271576@qq.com>
2024-09-30 19:38:27 +0800

    single mode

--- game/static/js/src/playground/zbase.js
+++ game/static/js/src/playground/zbase.js
@@ -4,13 +4,30 @@ class AcGamePlayground
     {
         this.root = root;
         this.$playground = $(`
-<div>
-    游戏界面
+<div class="ac-game-playground">
+
 </div>
         `);
-        this.hide();
+        //this.hide();
         this.root.$ac_game.append(this.$playground);
+        this.width = this.$playground.width();
+        this.height = this.$playground.height();
+        this.game_map = new GameMap(this);
+        this.players = [];
+        this.players.push(new Player(this, this.width / 2, this.height / 2, this.height * 0.05, "white", this.height * 0.7, true));
+
+        for (let i = 0; i < 3; i ++)
+        {
+            this.players.push(new Player(this, this.width / 2, this.height / 2, this.height * 0.05, this.get_random_color(), this.height * 0.7, false));
+        }
+
         this.start();
+    }
+
+    get_random_color()
+    {
+        let colors = ["blue", "red", "pink", "green", "grey"];
+        return colors[Math.floor(Math.random() * 5)];
     }
 
     start()
